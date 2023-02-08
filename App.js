f38bf0140174bda1cca8edcb1962f9db8d4aa1cb
Frank Cairampoma <ingfralch@gmail.com>
2023-02-08 17:59:22 -0500

    diseño

--- App.js
+++ App.js
@@ -2,7 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
+  function solveEquation(equation) {
+    let terms = equation.split("=")[0].split("+");
+    let constant = equation.split("=")[1];
+    let xCoeff = 0;
+    for (let i = 0; i < terms.length; i++) {
+      let term = terms[i].trim();
+      if (term.includes("x")) {
+        xCoeff += parseInt(term.replace("x", ""));
+      } else {
+        constant -= parseInt(term);
+      }
+    }
+    return "x = " + (constant / xCoeff);
+  }
+  
+  let equation = "2x - 3 = 7";
+  console.log(solveEquation(equation));
+
+    return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
