1b4dbe93d0f89d5ebc6334132ac958396b0768eb
Frank Cairampoma <ingfralch@gmail.com>
2023-02-10 15:45:21 -0500

    bloqueasr botones

--- components/IngresoDatos.jsx
+++ components/IngresoDatos.jsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity , ScrollView} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 
 import { Ionicons } from '@expo/vector-icons'; 
 
@@ -8,39 +8,48 @@ const windowHeight = Dimensions.get('window').height;
 
 
 function IngresoDatos() {
+    const [buttonsEnabled, setButtonsEnabled] = useState(true);
+
+    const bloqueandoBotones = (event) => {
+        setButtonsEnabled(false);
+    }
+    const desbloqueandoBotones = (event) => {
+        setButtonsEnabled(true);
+    }
+
     return (
         <View style={styles.container}>
              <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginLeft: 20 }}>
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                    <TouchableOpacity style={styles.boton}>
-                        <Text style={{ fontSize: 20, color: 'red' }}>C</Text>
+                <ScrollView horizontal={true} onScroll={bloqueandoBotones} showsHorizontalScrollIndicator={false} onMomentumScrollEnd ={desbloqueandoBotones}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                        <Text style={{ fontSize: 28, color: 'red' }}>C</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton}>
-                        <Text style={{ fontSize: 20 }}><Ionicons name="arrow-back" size={22} color="#0ffc03" /></Text>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                        <Text style={{ fontSize: 28 }}><Ionicons name="arrow-back" size={22} color="#0ffc03" /></Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton}>
-                        <Text style={{ fontSize: 20, color: '#0ffc03' }}>%</Text>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                        <Text style={{ fontSize: 28, color: '#0ffc03' }}>%</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton}>
-                        <Text style={{ fontSize: 20, color: '#0ffc03' }}>/</Text>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                        <Text style={{ fontSize: 28, color: '#0ffc03' }}>/</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton}>
-                        <Text style={{ fontSize: 20, color: '#0ffc03' }}>√</Text>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                        <Text style={{ fontSize: 28, color: '#0ffc03' }}>√</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton}>
-                        <Text style={{ fontSize: 20, color: '#0ffc03' }}>x!</Text>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                        <Text style={{ fontSize: 30, color: '#0ffc03' }}>^</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton}>
-                        <Text style={{ fontSize: 20, color: '#0ffc03' }}>π</Text>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                        <Text style={{ fontSize: 28, color: '#0ffc03' }}>x!</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton}>
-                        <Text style={{ fontSize: 19, color: '#0ffc03' }}>lg</Text>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                        <Text style={{ fontSize: 28, color: '#0ffc03' }}>π</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton}>
-                        <Text style={{ fontSize: 19, color: '#0ffc03' }}>ln</Text>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                        <Text style={{ fontSize: 25, color: '#0ffc03' }}>lg</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton}>
-                        <Text style={{ fontSize: 19, color: '#0ffc03' }}>xy</Text>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                        <Text style={{ fontSize: 25, color: '#0ffc03' }}>ln</Text>
                     </TouchableOpacity>
                 </ScrollView>
                
@@ -49,58 +58,58 @@ function IngresoDatos() {
 
             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                 <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: 'white' }}>7</Text>
+                    <Text style={{ fontSize: 28, color: 'white' }}>7</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: 'white' }}>8</Text>
+                    <Text style={{ fontSize: 28, color: 'white' }}>8</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: 'white' }}>9</Text>
+                    <Text style={{ fontSize: 28, color: 'white' }}>9</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: '#0ffc03' }}>x</Text>
-                </TouchableOpacity>
-
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: 'white' }}>4</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: 'white' }}>5</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: 'white' }}>6</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: '#0ffc03' }}>-</Text>
+                    <Text style={{ fontSize: 28, color: '#0ffc03' }}>x</Text>
                 </TouchableOpacity>
 
             </View>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                 <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: 'white' }}>1</Text>
+                    <Text style={{ fontSize: 28, color: 'white' }}>4</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: 'white' }}>2</Text>
+                    <Text style={{ fontSize: 28, color: 'white' }}>5</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: 'white' }}>3</Text>
+                    <Text style={{ fontSize: 28, color: 'white' }}>6</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.boton}>
-                    <Text style={{ fontSize: 20, color: '#0ffc03' }}>+</Text>
+                    <Text style={{ fontSize: 28, color: '#0ffc03' }}>-</Text>
+                </TouchableOpacity>
+
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <TouchableOpacity style={styles.boton}>
+                    <Text style={{ fontSize: 28, color: 'white' }}>1</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.boton}>
+                    <Text style={{ fontSize: 28, color: 'white' }}>2</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.boton}>
+                    <Text style={{ fontSize: 28, color: 'white' }}>3</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.boton}>
+                    <Text style={{ fontSize: 28, color: '#0ffc03' }}>+</Text>
                 </TouchableOpacity>
 
             </View>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom:10 }}>
                 <TouchableOpacity style={styles.boton}>
-                   <Text style={{ fontSize: 20, color: 'white' }}>.</Text>
+                   <Text style={{ fontSize: 28, color: 'white' }}>.</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.boton}>
-                   <Text style={{ fontSize: 20, color: 'white' }}>0</Text>
+                   <Text style={{ fontSize: 28, color: 'white' }}>0</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={[styles.boton, {width: windowWidth * 0.4}]}>
-                   <Text style={{ fontSize: 20, color: 'red' }}>{"="}</Text>
+                   <Text style={{ fontSize: 28, color: 'red' }}>{"="}</Text>
                 </TouchableOpacity>
 
             </View>
