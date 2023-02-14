73cb458782709ce9e9975e53367090db9d64cc27
Frank Cairampoma <ingfralch@gmail.com>
2023-02-14 18:29:20 -0500

    guardando datos en arreglo

--- components/IngresoDatos.jsx
+++ components/IngresoDatos.jsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity , ScrollView} from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import { Ionicons } from '@expo/vector-icons'; 
 
@@ -9,6 +9,7 @@ const windowHeight = Dimensions.get('window').height;
 
 function IngresoDatos() {
     const [buttonsEnabled, setButtonsEnabled] = useState(true);
+    const [datos , setDatos] = useState([]);
 
     const bloqueandoBotones = (event) => {
         setButtonsEnabled(false);
@@ -16,39 +17,44 @@ function IngresoDatos() {
     const desbloqueandoBotones = (event) => {
         setButtonsEnabled(true);
     }
+    useEffect(() => {
+        console.log(datos);
+    }, [datos])
+
+  
 
     return (
         <View style={styles.container}>
              <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginLeft: 20 }}>
                 <ScrollView horizontal={true} onScroll={bloqueandoBotones} showsHorizontalScrollIndicator={false} onMomentumScrollEnd ={desbloqueandoBotones}>
-                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled} onPress={() => {setDatos([...datos, "C"]);}}>
                         <Text style={{ fontSize: 28, color: 'red' }}>C</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled} onPress={() => {setDatos([...datos, "back"]);}}>
                         <Text style={{ fontSize: 28 }}><Ionicons name="arrow-back" size={22} color="#0ffc03" /></Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled} onPress={() => {setDatos([...datos, "%"]);}}>
                         <Text style={{ fontSize: 28, color: '#0ffc03' }}>%</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled} onPress={() => {setDatos([...datos, "/"]);}}>
                         <Text style={{ fontSize: 28, color: '#0ffc03' }}>/</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled} onPress={() => {setDatos([...datos, "√"]);}}>
                         <Text style={{ fontSize: 28, color: '#0ffc03' }}>√</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled} onPress={() => {setDatos([...datos, "^"]);}}>
                         <Text style={{ fontSize: 30, color: '#0ffc03' }}>^</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled} onPress={() => {setDatos([...datos, "x!"]);}}>
                         <Text style={{ fontSize: 28, color: '#0ffc03' }}>x!</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled} onPress={() => {setDatos([...datos, "π"]);}}>
                         <Text style={{ fontSize: 28, color: '#0ffc03' }}>π</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled} onPress={() => {setDatos([...datos, "lg"]);}}>
                         <Text style={{ fontSize: 25, color: '#0ffc03' }}>lg</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled}>
+                    <TouchableOpacity style={styles.boton} disabled={!buttonsEnabled} onPress={() => {setDatos([...datos, "ln"]);}}>
                         <Text style={{ fontSize: 25, color: '#0ffc03' }}>ln</Text>
                     </TouchableOpacity>
                 </ScrollView>
@@ -57,58 +63,58 @@ function IngresoDatos() {
             </View>
 
             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton}  onPress={() => {setDatos([...datos, 7]);}} >
                     <Text style={{ fontSize: 28, color: 'white' }}>7</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, 8]);}}>
                     <Text style={{ fontSize: 28, color: 'white' }}>8</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, 9]);}}>
                     <Text style={{ fontSize: 28, color: 'white' }}>9</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, "x"]);}}>
                     <Text style={{ fontSize: 28, color: '#0ffc03' }}>x</Text>
                 </TouchableOpacity>
 
             </View>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, 4]);}}>
                     <Text style={{ fontSize: 28, color: 'white' }}>4</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, 5]);}}>
                     <Text style={{ fontSize: 28, color: 'white' }}>5</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, 6]);}}>
                     <Text style={{ fontSize: 28, color: 'white' }}>6</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, "-"]);}}>
                     <Text style={{ fontSize: 28, color: '#0ffc03' }}>-</Text>
                 </TouchableOpacity>
 
             </View>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, 1]);}}>
                     <Text style={{ fontSize: 28, color: 'white' }}>1</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, 2]);}}>
                     <Text style={{ fontSize: 28, color: 'white' }}>2</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, 3]);}}>
                     <Text style={{ fontSize: 28, color: 'white' }}>3</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, "+"]);}}>
                     <Text style={{ fontSize: 28, color: '#0ffc03' }}>+</Text>
                 </TouchableOpacity>
 
             </View>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom:10 }}>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, "."]);}}>
                    <Text style={{ fontSize: 28, color: 'white' }}>.</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.boton}>
+                <TouchableOpacity style={styles.boton} onPress={() => {setDatos([...datos, 0]);}}>
                    <Text style={{ fontSize: 28, color: 'white' }}>0</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={[styles.boton, {width: windowWidth * 0.4}]}>
+                <TouchableOpacity style={[styles.boton, {width: windowWidth * 0.4}]} onPress={() => {setDatos([...datos, "="]);}}>
                    <Text style={{ fontSize: 28, color: 'red' }}>{"="}</Text>
                 </TouchableOpacity>
 
